35dfe54f3bbe13a7568080a2036224fbfab77e52
Jaheim Dazzell <145047102+Dan-Dazzell@users.noreply.github.com>
2025-11-24 00:26:26 -0400

    Finished JS

--- world.js
+++ world.js
@@ -1,18 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const result = document.getElementById("result");
-    const input = document.getElementById("country");
+window.onload = () => {
+  const lookupBtn = document.getElementById("lookup");
+  const lookupCitiesBtn = document.getElementById("lookup-cities");
+  const result = document.getElementById("result");
+  const input = document.getElementById("country");
 
-    document.getElementById("lookup").addEventListener("click", () => {
-        const c = input.value.trim();
-        fetch("world.php?country=" + encodeURIComponent(c) + "&lookup=countries")
-            .then(r => r.text())
-            .then(t => result.innerHTML = t);
-    });
+  lookupBtn.onclick = () => {
+    fetch(`world.php?country=${input.value}`)
+      .then(res => res.text())
+      .then(data => result.innerHTML = data);
+  };
 
-    document.getElementById("lookup-cities").addEventListener("click", () => {
-        const c = input.value.trim();
-        fetch("world.php?country=" + encodeURIComponent(c) + "&lookup=cities")
-            .then(r => r.text())
-            .then(t => result.innerHTML = t);
-    });
-});
+  lookupCitiesBtn.onclick = () => {
+    fetch(`world.php?country=${input.value}&lookup=cities`)
+      .then(res => res.text())
+      .then(data => result.innerHTML = data);
+  };
+};
